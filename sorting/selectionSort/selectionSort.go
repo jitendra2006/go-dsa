0bc24296448846e8a9b6eb84bcffc14633e76611
Jitendra dwivedi <jitendra.dwivedi@realogy.com>
2023-11-29 19:47:24 +0530

    selection sort implementation

--- sorting/selectionSort/selectionSort.go
+++ sorting/selectionSort/selectionSort.go
@@ -3,5 +3,22 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Selection Sort")
+	inp := []int{10, 1, 12, 2, 33, 45, 5, -3, 0}
+	selectionSort(inp)
+	fmt.Println("sorted array: ", inp)
+}
+
+func selectionSort(arr []int) {
+	// [10,1,12,2,33,45,5,-3,0]
+	for i := 0; i < len(arr)-1; i++ {
+		min := i
+		for j := i + 1; j < len(arr)-1; j++ {
+			if arr[min] > arr[j] {
+				min = j
+			}
+		}
+		temp := arr[i]
+		arr[i] = arr[min]
+		arr[min] = temp
+	}
 }
